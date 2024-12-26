753ebadc96b4f7e9679b7920828976596ae939e3
Ugo SH <ugosh16@gmail.com>
2024-12-26 23:18:03 +0100

    Setup front & back (psql)

---
+++ api/src/main/java/com/wicker/WickerApplication.java
@@ -0,0 +1,13 @@
+package com.wicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WickerApplication.class, args);
+	}
+
+}
